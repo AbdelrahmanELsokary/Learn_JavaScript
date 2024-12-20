63dd80354438b13d9ce7dcd63397ced7b8df7e25
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2024-12-20 19:43:29 +0200

    Edit: Comments To English

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,10 +6,10 @@
 
 //Integer
 let number = 0;
-console.log(number++); // 0 (يتم عرض القيمة ثم الزيادة)
+console.log(number++); // 0 (The value is displayed and then incremented)
 console.log(number); // 1
-console.log(++number); // 2 (يتم الزيادة ثم عرض القيمة)
-console.log(~number); // -3 (يتم الزيادة بالسالب ثم عرض القيم)
+console.log(++number); // 2 (The value is incremented and then displayed)
+console.log(~number); // -3 (The increment is done by negative and then the value is displayed)
 console.log(--number); //1
 console.log(-number); //-1
 console.log(~~number); //1
@@ -21,15 +21,15 @@ console.log(++number + 10); //12
 
 //Power
 console.log(10 ** 10); //10000000000
-//باقي القسمه
+//rest of the division
 console.log(20 % 3); //2
-//القسمه
+//The division
 console.log(50 / 5); //10
-//الضرب
+//Multiplication
 console.log(50 * 5); //250
-//الجمع
+//Addition
 console.log(100 + 100); //200
-//الطرح
+//subtraction
 console.log(100 - 100); //0
 
 //Floating
@@ -42,25 +42,25 @@ console.log(age >= 18); // true
 age = 15;
 console.log(age >= 18); // false
 
-//const الثوابت
+//Constants
 const PI = 3.14;
-console.log(PI); // حاجه ثابته مش هتتغير حتي الازل
+console.log(PI); // Fixed and will not change forever
 
 // عوامل المقارنة
-console.log(5 == "5"); // true (مقارنة القيمة فقط)
+console.log(5 == "5"); // true (Value comparison only)
 console.log(5 == 5); // true
-console.log(5 === "5"); // false (مقارنة القيمة والنوع)
+console.log(5 === "5"); // false (Compare value and type)
 
-//العوامل المنطقية
+//Boolean operators
 //-----------------//
 //OR//
-//مش لازم القيم كلها تتحقق عشان تعطي ممكن قيمه واحده بسtrue
+//It is not necessary for all values ​​to be true Only one value may be possible
 console.log(true || true); // true
 console.log(false || true); // true
 console.log(false || false || true); // false
 
 //AND
-//لازم القيم كلها تتحقق عشان تعطي true
+//All values ​​must be true for true
 console.log(true && true); // true
 console.log(false && true); // false
 console.log(false && false && true); // false
@@ -68,11 +68,11 @@ console.log(false && false && false); // false
 console.log(true && true && true); // true
 
 //NOT
-//بتعكس القيمه
+//reversing values
 console.log(!true); //false
 console.log(!false); //true
 
-//نفي النفي اثبات
+//Negation of negation is proof
 console.log(!!!false); //true
 console.log(!!!true); //false
 
